2070779ac436a6db01863b5fc4922827f8cdd69c
HaoxinMa <haoxinma@usc.edu>
2021-09-11 17:51:10 -0700

    q7q8 func name

--- src/lib/cpplib.cc
+++ src/lib/cpplib.cc
@@ -1,4 +1,5 @@
 #include "cpplib.h"
+
 #include "limits"
 
 // Question 1
@@ -6,51 +7,47 @@
 // You should write 4 functions
 
 // Question 3
-void CPPLib::SwapByRefernce(float &input1, float &input2){
-    // Please fill up this function.
+void CPPLib::SwapByRefernce(float& input1, float& input2) {
+  // Please fill up this function.
 }
-void CPPLib::SwapByPointer(float *input1, float *input2){
-    // Please fill up this function.
+void CPPLib::SwapByPointer(float* input1, float* input2) {
+  // Please fill up this function.
 }
 
 // Question 4
-void CPPLib::UniqueVectorNotBySet(std::vector<int> &input){
-    // Please fill up this function.
+void CPPLib::UniqueVectorNotBySet(std::vector<int>& input) {
+  // Please fill up this function.
 }
-void CPPLib::UniqueVectorBySet(std::vector<int> &input){
-    // Please fill up this function.
+void CPPLib::UniqueVectorBySet(std::vector<int>& input) {
+  // Please fill up this function.
 }
-void CPPLib::ReverseVector(std::vector<int> &input){
-    // Please fill up this function.
+void CPPLib::ReverseVector(std::vector<int>& input) {
+  // Please fill up this function.
 }
-void CPPLib::PrimeVector(std::vector<int> &input){
-    // Please fill up this function.
+void CPPLib::PrimeVector(std::vector<int>& input) {
+  // Please fill up this function.
 }
-std::vector<int> CPPLib::IntersectVectors(std::vector<int> &input1, std::vector<int> &input2){
-    // Please fill up this function.
+std::vector<int> CPPLib::IntersectVectors(std::vector<int>& input1, std::vector<int>& input2) {
+  // Please fill up this function.
 }
 
 // Question 5
-void CPPLib::ToUpper(std::string& input)
-{
-    // Please fill up this function.
+void CPPLib::ToUpper(std::string& input) {
+  // Please fill up this function.
 }
 
 // Question 6
-bool CPPLib::canBePalindrome(const std::string &str)
-{
-    // Please fill up this function.
-    return true;
+bool CPPLib::canBePalindrome(const std::string& str) {
+  // Please fill up this function.
+  return true;
 }
 
-// Question 7 
-std::vector<std::vector<std::string>> CPPLib::groupAnagrams(std::vector<std::string>& strs)
-{
-    // Please fill up this function.
+// Question 7
+void CPPLib::Rearrange(std::vector<int>& input, int k) {
+  // Please fill up this function.
 }
 
 // Question 8
-void CPPLib::kthPeek(std::vector<int> &input, int k)
-{
-    // Please fill up this function.
+bool CPPLib::Match(const std::string& pattern, const std::vector<std::string>& strs) {
+  // Please fill up this function.
 }
--- src/lib/cpplib.h
+++ src/lib/cpplib.h
@@ -10,32 +10,30 @@
  */
 class CPPLib {
  public:
-  //q1
+  // q1
   // Please create your functions here.
-  //q3
-  void SwapByRefernce(float &input1, float &input2);
-  void SwapByPointer(float *input1, float *input2);
+  // q3
+  void SwapByRefernce(float& input1, float& input2);
+  void SwapByPointer(float* input1, float* input2);
 
-  //q4
-  void UniqueVectorNotBySet(std::vector<int> &input);
-  void UniqueVectorBySet(std::vector<int> &input);
-  void ReverseVector(std::vector<int> &input);
-  void PrimeVector(std::vector<int> &input);
-  std::vector<int> IntersectVectors(std::vector<int> &input1, std::vector<int> &input2);
+  // q4
+  void UniqueVectorNotBySet(std::vector<int>& input);
+  void UniqueVectorBySet(std::vector<int>& input);
+  void ReverseVector(std::vector<int>& input);
+  void PrimeVector(std::vector<int>& input);
+  std::vector<int> IntersectVectors(std::vector<int>& input1, std::vector<int>& input2);
 
-  //q5
+  // q5
   void ToUpper(std::string& input);
 
-  //q6 
-  bool canBePalindrome(const std::string &str);
+  // q6
+  bool canBePalindrome(const std::string& str);
 
-  //q7
-  std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs);
+  // q7
+  void Rearrange(std::vector<int>& input, int k);
 
-  //q8
-  void kthPeek(std::vector<int> &input, int k);
-
-  private : 
+  // q8
+  bool Match(const std::string& pattern, const std::vector<std::string>& strs);
 };
 
 #endif
